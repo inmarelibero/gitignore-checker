39c8983558378e06091c00b13594db50ead41220
Emanuele Gaspari <inmarelibero@gmail.com>
2020-11-17 10:18:42 +0100

    added comment

--- tests/GitIgnoreCheckerTest.php
+++ tests/GitIgnoreCheckerTest.php
@@ -28,5 +28,17 @@ class GitIgnoreCheckerTest extends AbstractTestCase
         $this->assertFalse(
             $gitIgnoreChecker->isPathIgnored('/foo/bar')
         );
+
+        /**
+         * @todo restore these assertions: the prloblem is that the two resources '/foo/ignore_me' and '/ignored_foo'
+         * cannot be versioned and pushed in the repo, as they are correctly ignored by gut
+         */
+        //$this->assertTrue(
+        //    $gitIgnoreChecker->isPathIgnored('/foo/ignore_me')
+        //);
+        //
+        //$this->assertTrue(
+        //    $gitIgnoreChecker->isPathIgnored('/ignored_foo')
+        //);
     }
 }
